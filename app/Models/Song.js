dccace81d5f2eff83daa777cd5ffcae2384baf81
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2021-01-07 16:53:18 -0700

    added style

--- app/Models/Song.js
+++ app/Models/Song.js
@@ -13,11 +13,11 @@ export default class Song {
   get Template() {
     return `
     <div class = "row">
-    <div class="col text-center">
+    <div class="col text-center m-3 py-2 rounded border-dark bg-warning shadow">
         <h5>${this.title} - by ${this.artist}</h5>
         <img src="${this.albumArt}" alt="uh oh">
         <p>${this.album}</p>
-        <p>$${this.price} <button class = "btn btn-primary" onclick="app.songsController.addSong('${this.id}')">Add to Playlist</button></p>
+        <p>$${this.price} <button class = "btn btn-h btn-primary" onclick="app.songsController.addSong('${this.id}')">Add to Playlist</button></p>
         <audio controls><source src="${this.preview}"></audio>
     </div>
   </div>
@@ -27,13 +27,13 @@ export default class Song {
   get playlistTemplate() {
     return `
     <div class = "row">
-    <div class="col text-center rounded-circle border-danger border my-3 bg-warning shadow">
+    <div class="col text-center rounded-bottom rounded border-dark m-3 py-2 bg-warning shadow text-light">
         <h5>${this.title} - by ${this.artist}</h5>
         <p>${this.album}</p>
         <audio controls><source src="${this.preview}"></audio>
         <div class="row">
           <div class = "col text-center">
-          <button class = "btn btn-danger" onclick = "app.songsController.removeSong('${this.id}')">Delete</button>
+          <button class = "btn btn-h btn-danger" onclick = "app.songsController.removeSong('${this.id}')">Delete</button>
           </div>
         </div>
     </div>
